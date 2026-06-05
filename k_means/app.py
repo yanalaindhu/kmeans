75414fc317ec83897878b188e0af51f5f9144f32
Yanala Indhu <yanalaindhu11@gmail.com>
2026-06-05 10:27:37 +0530

    Update app.py

--- k_means/app.py
+++ k_means/app.py
@@ -1,6 +1,54 @@
-import streamlit as st
-import numpy as np
+import os
 import joblib
+import pandas as pd
+
+if (
+    not os.path.exists("models/kmeans_model.pkl")
+    or
+    not os.path.exists("models/scaler.pkl")
+):
+
+    from sklearn.cluster import KMeans
+    from sklearn.preprocessing import StandardScaler
+
+    df = pd.read_csv(
+        "data/Mall_Customers.csv"
+    )
+
+    X = df[
+        [
+            "Age",
+            "Annual Income (k$)",
+            "Spending Score (1-100)"
+        ]
+    ]
+
+    scaler = StandardScaler()
+
+    X_scaled = scaler.fit_transform(X)
+
+    model = KMeans(
+        n_clusters=5,
+        random_state=42,
+        n_init=10
+    )
+
+    model.fit(X_scaled)
+
+    os.makedirs(
+        "models",
+        exist_ok=True
+    )
+
+    joblib.dump(
+        model,
+        "models/kmeans_model.pkl"
+    )
+
+    joblib.dump(
+        scaler,
+        "models/scaler.pkl"
+    )
 
 model = joblib.load(
     "models/kmeans_model.pkl"
@@ -9,84 +57,3 @@ model = joblib.load(
 scaler = joblib.load(
     "models/scaler.pkl"
 )
-
-st.set_page_config(
-    page_title="Customer Segmentation",
-    page_icon="📊"
-)
-
-st.title(
-    "📊 Customer Segmentation using K-Means"
-)
-
-st.write(
-    "Predict customer cluster."
-)
-
-age = st.number_input(
-    "Age",
-    min_value=1,
-    max_value=100,
-    value=25
-)
-
-income = st.number_input(
-    "Annual Income (k$)",
-    min_value=1,
-    max_value=200,
-    value=50
-)
-
-score = st.number_input(
-    "Spending Score",
-    min_value=1,
-    max_value=100,
-    value=50
-)
-
-if st.button(
-    "Predict Cluster"
-):
-
-    data = np.array(
-        [
-            [age, income, score]
-        ]
-    )
-
-    data_scaled = scaler.transform(
-        data
-    )
-
-    cluster = model.predict(
-        data_scaled
-    )[0]
-
-    st.success(
-        f"Customer belongs to Cluster {cluster}"
-    )
-
-    if cluster == 0:
-        st.info(
-            "Average Customers"
-        )
-
-    elif cluster == 1:
-        st.info(
-            "High Spending Customers"
-        )
-
-    elif cluster == 2:
-        st.info(
-            "Premium Customers"
-        )
-
-    elif cluster == 3:
-        st.info(
-            "Budget Customers"
-        )
-
-    else:
-        st.info(
-            "Young Customers"
-        )
